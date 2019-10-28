245e8223ace1c939386fc4b33c63095316dd1ccd
Brittany Alexander <brittany@stat.tamu.edu>
2019-10-28 13:01:33 -0500

    implemented  estimation of MLE of normal and MOM of beta distribution.

---
+++ R/estimation.R
@@ -0,0 +1,37 @@
+#file to get the 
+
+#' MLE estimates of a Normal Distribution
+#' Fits a normal 
+#' @param data - a vector of data
+#'
+#' @return a list with the elements mean with the mean of the distribution and 
+#' @export
+#'
+#' @examples
+fitnormalvecMLE <- function(data){
+  if(!is.vector(data)){
+    stop("data must be a vector")
+  }
+  mean = mean(data)
+  var = (length(data) - 1)var(data)/length(data) # var uses n-1 so we must multiply by (n-1)/n to get MLE
+  return(list(mean = mean, var = var))
+}
+
+
+
+#' Beta Method of Moments
+#' Calculates the method of monements estimators of a beta distribution fitted to the data
+#' @param data - a vector of data 
+#'
+#' @return The fitted a,b of a beta(a,b) distribtion
+#' @export
+#'
+#' @examples
+fitbetavecMOM <-function(data){
+  
+  datamean =  mean(data)
+  datavar = var(data)
+  a = (datamean^2(1-datamean)-datavar*datamean)/datavar
+  b = a(1-datamean)/datamean
+  return(list(a = a, b = b))
+}
