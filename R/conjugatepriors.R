a7357fd6df4daca275db3498c98a51d16228e5c2
Brittany Alexander <brittany@stat.tamu.edu>
2019-11-20 13:37:46 -0600

    added documentation examples and fixed bugs discovering by testing the examples.

--- R/conjugatepriors.R
+++ R/conjugatepriors.R
@@ -4,29 +4,43 @@
 #'
 #' @param data a vector  of the data or a single point
 #' @param priormean mean of prior distribution
-#' @param priorvar variance of prior distribution
-#' @param datavar datavariance,  if null var is called to estimate it
-#' @param n sample size
+#' @param priorvar variance of prior distribution if invgamma is not true, shift parameter for variance if invgamma is true
+#' @param datavar data variance,  if null var is called to estimate it
+#' @param singlepoll if true it calculates the variance based on the sample size
+#' @param n approriate sample size
 #' @param invgamma a logical indicator of whether or not a inverse gamma prior for sigma is (T) or isn't included
-#' @param a0 prior a if gamma is T
-#' @param b0 prior b if gamma is T
-#' @param V0 prior V0 where the likelihood is N(priormean)
+#' @param a0 prior a if invgamma is T
+#' @param b0 prior b if invgamma is T
 #'
 #' @return
 #' @export
 #'
 #' @examples
-unigausscp = function(data, priormean, priorvar, datavar = NULL,  n = NULL, invgamma = F, V0 = NULL, a0 = NULL, b0 = NULL){
+#' set.seed(10)
+#' data1 = rnorm(30, mean = .48, sd = 0.05)
+#' n1 = floor(runif(1, 200, 800))
+#' datavar1 = mean(sqrt(data1[1]*(1-data1[1])/n1))
+#' unigausscp(data1, 0.5, 0.05)
+#' unigausscp(data1[1], 0.5, 0.05, singlepoll = T, n = n1)
+#' unigausscp(data1, 0.5, 0.05, invgamma = T, a0 = 0.0001, b0 = 0.0001)
+#' unigausscp(data1[1], 0.5, 0.05, singlepoll = T, n = n1, invgamma = T, a0 = 0.0001, b0 = 0.0001)
+#' 
+#' 
+unigausscp = function(data, priormean, priorvar, datavar = NULL, singlepoll = F,  n = NULL, invgamma = F, a0 = NULL, b0 = NULL){
   if(is.null(n)){
     n = length(data)
   }
   if(is.null(datavar)){
-    datavar = var(data)
+    if(singlepoll == T){
+      datavar = sqrt(data*(1-data)/n)
+    }
+    if(singlepoll == F){
+      datavar = var(data)
+    }
   }
   datamean =  mean(data)
   if(invgamma == F){
     #this does just a gaussian - gaussian conjugate prior assuming sigma is known or fixed
-    datavar = var(data)
     postmean = priormean * (datavar / (n * priorvar + datavar)) + datamean * (n * priorvar)/(n * priorvar + datavar)
     postvar = (n/datavar + 1/priorvar)^-1
     posta= NULL
@@ -35,15 +49,22 @@ unigausscp = function(data, priormean, priorvar, datavar = NULL,  n = NULL, invg
   
     }
   if(invgamma == T){
+    if(singlepoll == T){
+      ssqdata = round(datamean * n, 0)
+    }
     #adds a prior distribution and posterior of sigma
-    postvar = 1/(1/v0 + n)
-    postmean = (priormean/priorvar + n * datamean)/postV
+    if(singlepoll == F){
+      ssqdata = sum(data^2)
+      }
+    postv = 1/(1/priorvar + n)
+    postmean = (priormean/priorvar + n * datamean) * postv
     posta = a0 + n/2
-    postb = b0 + 0.5*(priormean^2/priorvar+sum(data^2)-postmean^2/postvar)
-    dataweight = (n/postV)/(n/postV + (priorvar^-1)/postvar)
+    postb = b0 + 0.5 * (priormean^2 /priorvar + ssqdata - postmean^2/postv)
+    postvar = postb / ((posta - 1)*postv)
+    dataweight = (n/postv)/(n/postv + (priorvar^-1)/postvar)
   }
-  sdpost = sqrt(postvar)
-  return(list(postmean = postmean, postvar = postvar, postsd = postsd, posta = posta,  postb = postb, dataweight = dataweight))
+  postsd = sqrt(postvar)
+  return(list(priormean  = priormean,  priorvar = priorvar, a0 = a0, b0 = b0,  n = n, datavar = datavar, invgamma = invgamma, postmean = postmean, postvar = postvar, postsd = postsd, posta = posta,  postb = postb, dataweight = dataweight))
 }
 
 
@@ -56,7 +77,6 @@ unigausscp = function(data, priormean, priorvar, datavar = NULL,  n = NULL, invg
 #' @param datavar a vector of the variance for each data point
 #' @param n a vector of sample sizes
 #' @param invgamma a logical indicator of whether or not a inverse gamma prior for sigma is (T) or isn't included
-#' @param V0 initial v0
 #' @param a0 initial a0
 #' @param b0 inital b0
 #'
@@ -64,11 +84,16 @@ unigausscp = function(data, priormean, priorvar, datavar = NULL,  n = NULL, invg
 #' @export
 #'
 #' @examples
-unigausscpiterative = function(data, priormean, priorvar, datavar = NULL, n, invgamma = F, V0 = NULL, a0 = NULL, b0 = NULL){
-  dataweight = rep(0, length(data))
+#' set.seed(12)
+#' data1 = c(rnorm(10, mean = .48, sd = 0.05), rnorm(20, mean = .49, sd = 0.05),  rnorm(20, mean = .51, sd = 0.05))
+#' n = floor(runif(50, 200, 800))
+#' unigausscpiterative(data1, 0.5, 0.05, n = n) 
+#' unigausscpiterative(data1, 0.5, 0.05, n = n, invgamma = T, a0 = 0.0001, b0 = 0.0001)
+unigausscpiterative = function(data, priormean, priorvar, datavar = NULL, n, invgamma = F,  a0 = NULL, b0 = NULL){
+  dataweights = rep(0, length(data))
   postmeans = rep(0, length(data))
-  postvar = rep(0, length(data))
-  postsd = rep(0, length(data))
+  postvars = rep(0, length(data))
+  postsds = rep(0, length(data))
   posta = rep(a0, length(data))
   postb = rep(b0, length(data))
   if(is.null(datavar)){
@@ -77,31 +102,39 @@ unigausscpiterative = function(data, priormean, priorvar, datavar = NULL, n, inv
   if(invgamma == F){
     for(i in 1:length(data)){
       out = unigausscp(data[i], priormean, priorvar, datavar = datavar[i], n[i])
-      postmeans[i] = out$priormean
-      postvar[i] = out$priorvar
-      priorsd[i] = out$priorsd
-      dataweight[i] = out$dataweight
-      priormean = out$priormean
-      priorvar = out$priorvar
+      postmeans[i] = out$postmean
+      postvars[i] = out$postvar
+      postsds[i] = out$postsd
+      dataweights[i] = out$dataweight
+      priormean = out$postmean
+      priorvar = out$postvar
       
     }
     
   }
   if(invgamma == T){
-    for(u in 1:length(data)){
-      out = unigausscp(data[i], priormean, priorvar, datavar, n, invgamma, V0, a0, b0)
-      postmeans[i] = out$priormean
-      postvar[i] = out$priorvar
-      priorsd[i] = out$priorsd
+    for(i in 1:length(data)){
+      out = unigausscp(data[i], priormean, priorvar, datavar = datavar[i], singlepoll = T, n = n[i], invgamma, a0, b0)
+      postmeans[i] = out$postmean
+      postvars[i] = out$postvar
+      postsds[i] = out$postsd
       posta[i] = out$posta
       postb[i] = out$postb
-      dataweight[i] = out$dataweight
-      priormean = out$priormean
-      priorvar = out$priorvar
+      dataweights[i] = out$dataweight
+      priormean = out$postmean
+      priorvar = out$postvar
+      a0 = out$posta
+      b0 = out$postb
+      
       
       
     }
+
   }
+  finalpostmean = priormean
+  finalpostvar = priorvar
+  finalpostsd = sqrt(priorvar)
+  return(list(finalpostmean = finalpostmean,  finalpostvar = finalpostvar, finalpostsd = finalpostsd, dataweights = dataweights, postmeans = postmeans, postvars = postvars, postsds = postsds, posta = posta, postb = postb))
 }
 
 
