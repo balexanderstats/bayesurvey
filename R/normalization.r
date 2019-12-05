e99004912338a85056fbfa277aff76eb7534e09a
Brittany Alexander <brittany@stat.tamu.edu>
2019-12-05 14:40:04 -0600

    added documentation to propnormdfreplace.

--- R/normalization.r
+++ R/normalization.r
@@ -25,7 +25,7 @@ propnorm = function(data){
   return(normdata)
 }
 
-#' Title
+#' Proportional Normalization of Data Frame
 #'
 #' @param df data frame to be  
 #' @param columns a vector of columns to be normalized
@@ -47,6 +47,19 @@ propnormdf = function(df, columns){
 }
 
 
+#' Title
+#' @param df data frame to be  
+#' @param columns a vector of columns to be normalized
+#'
+#' @return a new data frame with both original and the normalized data with column names ending in normalized
+#' @export
+#'
+#' @examples
+#' df1 = data.frame(a = c(0.4, 0.3, .35, .45), b = c(.45, .40, .40, .3))
+#' propnormdfreplace(df1)
+#' 
+#' df2  = data.frame(a = c(0.3, .34, .38), b = c(0.2, 0.21, 0.25), c = c(.45, .42, .41))
+#' propnormdfreplace(df2)
 propnormdfreplace = function(df, columns){
   normdata = propnorm(df[, columns])
   df[, columns] = normdata
